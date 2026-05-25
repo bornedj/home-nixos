5aad1e8aeeef43bcd9a3caf52a36682cf7db2765
Daniel Borne <borne.danielj@gmail.com>
2026-05-25 14:26:54 -0400

    build: nvidia driver settings

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -28,4 +28,16 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # nvidia setup
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = false;
+      powerManagement.finegrained = false;
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 }
