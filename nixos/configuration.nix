66a04003f8f357bf93fb87c392f0053a6032782b
Daniel Borne <borne.danielj@gmail.com>
2026-05-25 16:23:51 -0400

    refactor: use the new arg names for desktop manager

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,4 +1,5 @@
 # Edit this configuration file to define what should be installed on
+
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
@@ -46,8 +47,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
