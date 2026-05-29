c5adfdbcd6966a364a3cd68f38f9b2a0aa72fccd
Daniel Borne <borne.danielj@gmail.com>
2026-05-28 21:29:49 -0400

    chore: still bad autostart

--- home/programs/hyprland/modules/autostart.lua
+++ home/programs/hyprland/modules/autostart.lua
@@ -3,14 +3,7 @@ local vars = require("modules.vars")
 -- autostart
 hl.on("hyprland.start", function ()
     hl.exec_cmd("hyprpaper")
+
     hl.exec_cmd(vars.terminal)
-
-    -- setup the vert monitor
-    -- hl.exec_cmd("hyprctl [workspace name:".. vars.workspace.browser .. " silent] " .. vars.browser)
-    -- hl.exec_cmd("hyprctl [workspace name:".. vars.workspace.discord .. " silent] " .. vars.discord)
-
-    -- setup horizontal
-    -- hl.exec_cmd("hyprctl [workspace name:".. vars.workspace.terminal .. " silent] " .. vars.terminal)
-    -- hl.exec_cmd("hyprctl [workspace name:".. vars.workspace.steam .. " silent] " .. vars.steam)
-
+    hl.exec_cmd(vars.browser)
 end)
