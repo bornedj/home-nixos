f86423feab21e1282de27003412a3989300bb20a
Daniel Borne <borne.danielj@gmail.com>
2026-05-29 21:09:16 -0400

    chore: remove from autostart since its done at build

--- home/programs/hyprland/modules/autostart.lua
+++ home/programs/hyprland/modules/autostart.lua
@@ -3,7 +3,6 @@ local vars = require("modules.vars")
 -- autostart
 hl.on("hyprland.start", function ()
     hl.exec_cmd("hyprpaper")
-    h1.exec_cmd("matugen image ~/Pictures/wallpapers/vagabond-water.jpg -b wal")
 
     hl.exec_cmd(vars.terminal)
     hl.exec_cmd(vars.browser)
