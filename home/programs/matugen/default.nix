56b15af023566210f14cbe54dcc5888650d9d89e
Daniel Borne <borne.danielj@gmail.com>
2026-05-29 20:25:20 -0400

    fix: matugen setup

--- home/programs/matugen/default.nix
+++ home/programs/matugen/default.nix
@@ -1,5 +1,22 @@
-{ ... }:
+{ inputs, ... }:
 {
+    imports = [
+        inputs.matugen.nixosModules.default
+    ];
+
+    programs.matugen = {
+        variant = "dark";
+        wallpaper = "~/Pictures/wallpapers/vagabond-water.jpg";
+        jsonFormat = "hex";
+
+        templates = {
+            ghostty = {
+                input_path = "~/.config/matugen/templates/musashi.ghostty";
+                output_path = "~/.config/ghostty/themes/musashi.ghostty";
+            };
+        };
+    };
+
     home.file.".config/matugen" = {
         source = ./modules;
         recursive = true;
