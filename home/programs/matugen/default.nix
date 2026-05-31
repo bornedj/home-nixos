92a5614ebaeb7e664d080e828939ccaa20c27b22
Daniel Borne <borne.danielj@gmail.com>
2026-05-31 12:26:32 -0400

    refactor: attempt to style tmux with md3

--- home/programs/matugen/default.nix
+++ home/programs/matugen/default.nix
@@ -21,10 +21,10 @@
                 output_path = "~/.config/matugen.lua";
                 post_hook = "pkill -SIGUSR1 nvim";
             };
-            dotbar = {
-                input_path = ./templates/statusbar.conf;
-                output_path = "~/.config/statusbar.conf";
-                post_hook = "tmux source ~/.config/tmux/tmux.conf";
+            tmux = {
+                input_path = ./templates/tmux-colors.conf;
+                output_path = "~/.config/tmux-colors.conf";
+                post_hook = "tmux source ~/.config/tmux/tmux-colors.conf";
             };
             quickshell = {
                 input_path = ./templates/quickshell.json;
@@ -34,6 +34,6 @@
     };
     home.file.".config/ghostty/themes/musashi".source = "${config.programs.matugen.theme.files}/.config/ghostty/themes/musashi";
     home.file.".config/nvim/lua/matugen.lua".source = "${config.programs.matugen.theme.files}/.config/matugen.lua";
-    home.file.".config/tmux/statusbar.conf".source = "${config.programs.matugen.theme.files}/.config/statusbar.conf";
+    home.file.".config/tmux/tmux-colors.conf".source = "${config.programs.matugen.theme.files}/.config/tmux-colors.conf";
     home.file.".local/state/quickshell/generated/colors.json".source = "${config.programs.matugen.theme.files}/.config/quickshell.json";
 }
