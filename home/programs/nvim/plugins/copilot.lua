63227b04feb9794c7d0624f2ecc4f9b01e01582e
Daniel Borne <borne.danielj@gmail.com>
2026-07-13 21:09:52 -0400

    fix: copilot bindings

--- home/programs/nvim/plugins/copilot.lua
+++ home/programs/nvim/plugins/copilot.lua
@@ -1,5 +1,5 @@
-vim.keymap.set('n', "<leader>cpe", vim.cmd(":Copilot enable"))
-vim.keymap.set('n', "<leader>cpd", vim.cmd(":Copilot disable"))
+vim.keymap.set('n', "<leader>cpe", vim.cmd { cmd = "Copilot", args = {"enable"} })
+vim.keymap.set('n', "<leader>cpd", vim.cmd { cmd = "Copilot", args = {"enable"} })
 vim.keymap.set('i', '<C-y>', 'copilot#Accept("")', {
   expr = true,
   replace_keycodes = false
